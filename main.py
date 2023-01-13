3f35a0f83814661fb403e17ba17f65a946c472be
Piotr Smarzynski <piotr.smarzynski@swissrotors.com>
2023-01-13 11:07:15 +0100

    polar coordinates

--- main.py
+++ main.py
@@ -4,28 +4,28 @@ from player_class import Player_Class
 
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
+KOLOR = (255, 0, 0)
 
 pygame.init()
 # definiowanie okna gry
 win = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 # wyświetlanie okna gry
 pygame.display.set_caption("Moja Gra")
-player = Player_Class(width=100, height=100, pos_x=0, pos_y=0, rotation=0, window_size=(SCREEN_WIDTH, SCREEN_HEIGHT))
 
+img_path = 'img/player1.png'
+playerImg = pygame.image.load(img_path)
+
+player = Player_Class(parent=win, width=100, height=100, pos_x=0, pos_y=0, rotation=0, window_size=(SCREEN_WIDTH, SCREEN_HEIGHT), color=KOLOR, img=playerImg)
+playerImg = pygame.transform.scale(playerImg, (player.width, player.height))
 
 run = True
-# pętla główna
-#definiujemy zmienne
-KOLOR = (255, 0, 0)
-x = 10
-y = 30
-rect_width = 100
-rect_height = 100
 rect_small = False
-krok = 1
+
+player.pos_step = 0.2
 
 rect_pulse_timer = now()
 screen_refresh_timer = now()
+print_debug_timer = now()
 
 while run:
     win.fill((0,0,0))
@@ -35,18 +35,31 @@ while run:
             run = False
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] :
-        player.move(-player.pos_step, 0)
+    if True in keys:
+        if keys[pygame.K_LEFT] :
+            # player.set_pos(-player.pos_step, 0)
+            player.rotate("R")            
+
+        if keys[pygame.K_RIGHT] :
+            # player.set_pos(player.pos_step, 0)
+            player.rotate("L")
+
+        if keys[pygame.K_UP] :
+            # player.set_pos(0, -player.pos_step)
+            player.move('FWD')
+
+        if keys[pygame.K_DOWN] :
+            # player.set_pos(0, player.pos_step)
+            player.move('BWD')
+
+        # if keys[pygame.K_RCTRL] :
+        #     # player.rotate("R")
+
+        # if keys[pygame.K_KP0] :
+        #     # player.rotate("L")
+
         pygame.time.delay(1)
-    if keys[pygame.K_RIGHT] :
-        player.move(player.pos_step, 0)
-        pygame.time.delay(1)
-    if keys[pygame.K_UP] :
-        player.move(0, -player.pos_step)
-        pygame.time.delay(1)
-    if keys[pygame.K_DOWN] :
-        player.move(0, player.pos_step)
-        pygame.time.delay(1)
+
 
 
     if actual_time - rect_pulse_timer > 1:
@@ -60,9 +73,18 @@ while run:
         player.width = 100
         player.height = 90
 
-    pygame.draw.rect(win, KOLOR, (player.pos_x, player.pos_y, player.width, player.height))
+
+    if actual_time - print_debug_timer > 0.5:
+        print_debug_timer = actual_time
+        # print('rotation:', player.rotation)
+
+
 
     if actual_time - screen_refresh_timer > 1/60:
         screen_refresh_timer = actual_time
         
+        playerImg = pygame.image.load(img_path)
+        playerImg = pygame.transform.scale(playerImg, (player.width, player.height))
+        playerImg = pygame.transform.rotate(playerImg, player.rotation)
+        win.blit(playerImg, (player.pos_x, player.pos_y))
         pygame.display.update()
--- player_class.py
+++ player_class.py
@@ -1,17 +1,22 @@
+import math
+
 class Player_Class():
-    def __init__(self, height, width, pos_x, pos_y, rotation, window_size):
+    def __init__(self, parent, window_size, height, width, pos_x, pos_y, rotation, color, img):
+        self.parent = parent
         self.height = height
         self.width = width
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.pos_step = 1
+        self.rotation_step = 0.5
         self.rotation = rotation
         self.window_size = window_size
+        self.color = color
+        self.img = img
+        self.vector = [0, 0]
+        self.check_boundaries()
 
-    def move(self, x, y):        
-        self.pos_x += x
-        self.pos_y += y
-
+    def check_boundaries(self):
         if self.pos_x > self.window_size[0] - 120:
             self.pos_x = self.window_size[0] - 120
 
@@ -24,5 +29,36 @@ class Player_Class():
         elif self.pos_y < 20:
             self.pos_y = 20
 
-    def rotate(self, angle):
-        self.rotation += angle
+        if self.rotation > 360:
+            self.rotation -= 360
+
+        elif self.rotation < 0:
+            self.rotation += 360
+
+    def set_pos(self, x, y):        
+        self.pos_x += x
+        self.pos_y += y
+        self.check_boundaries()
+
+    def rotate(self, dir):
+        if dir == "L":
+            self.rotation -= self.rotation_step
+        else:
+            self.rotation += self.rotation_step
+
+        self.check_boundaries()
+
+
+    def move(self, direction):
+        self.vector = [0, 0]
+        self.vector[0] = self.pos_step * math.cos(math.radians(self.rotation - 90))
+        self.vector[1] = self.pos_step * math.sin(math.radians(self.rotation - 90))
+
+        if direction == 'FWD':            
+            self.pos_x -= self.vector[0]
+            self.pos_y += self.vector[1]
+        else:            
+            self.pos_x += self.vector[0]
+            self.pos_y -= self.vector[1]
+
+        self.check_boundaries()        
